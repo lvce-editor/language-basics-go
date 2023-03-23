86489b201f17109f00c5c0a4b9b98d89b4a9705b
Le Vivilet <levivlet@pm.me>
2023-03-23 20:11:28 +0100

    test: add switch and modulo tests

---
+++ test/cases/if-else.go
@@ -0,0 +1,5 @@
+if 7%2 == 0 {
+	fmt.Println("7 is even")
+} else {
+	fmt.Println("7 is odd")
+}
---
+++ test/cases/switch-time.go
@@ -0,0 +1,6 @@
+switch time.Now().Weekday() {
+case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+default:
+		fmt.Println("It's a weekday")
+}
